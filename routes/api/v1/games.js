7aebdf57c53fb97cc96fba6f128668abd9a6716d
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-27 13:34:30 -0600

    Adds get all games to games.js

---
+++ routes/api/v1/games.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var Game = require('../../../models').Game;
+
+router.get("/", function(req, res, next) {
+  Game.findAll()
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+module.export = router;
